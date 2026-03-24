7c67fe1556bb284482a498b245a5bfdf47cd379b
Sneaky26 <justinedimla000@gmail.com>
2026-03-24 23:53:52 +0800

    fixed the database table concerning regform data collection

--- model/schema.sql
+++ model/schema.sql
@@ -1,33 +1,41 @@
--- Create Database
-CREATE DATABASE IF NOT EXISTS VotingSystem;
+CREATE DATABASE VotingSystem;
 USE VotingSystem;
 
--- Roles Table
-CREATE TABLE `Roles` (
+CREATE TABLE `VotingSystem`.`Roles` (
   `role_id` INT NOT NULL AUTO_INCREMENT, 
   `role_name` VARCHAR(255) NOT NULL, 
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000;
+) ENGINE = InnoDB AUTO_INCREMENT = 1000;
 
--- Users Table 
-CREATE TABLE `Users` (
+
+CREATE TABLE `VotingSystem`.`Users` (
     `user_id` INT NOT NULL AUTO_INCREMENT,
-    `student_id` VARCHAR(20) NOT NULL UNIQUE,
-    `username` VARCHAR(255) NOT NULL UNIQUE,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `student_id` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `role_id` INT NOT NULL,
+    `activated_status` TINYINT(1) NOT NULL DEFAULT 0,
     `created_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_id`),
     FOREIGN KEY (`role_id`) REFERENCES `Roles`(`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000;
+) ENGINE = InnoDB AUTO_INCREMENT = 1000;
 
--- Insert Roles
-INSERT INTO `Roles` (`role_name`) VALUES ('admin');
-INSERT INTO `Roles` (`role_name`) VALUES ('student');
+-- Roles
+INSERT INTO Roles(role_name) VALUES ("admin");
+INSERT INTO Roles(role_name) VALUES ('student_voter');
 
--- Insert Sample User
-INSERT INTO `Users`
-(`student_id`, `username`, `email`, `password`, `role_id`) 
-VALUES 
-('20240000001', 'tephL', 'tephl@gmail.com', '123456', 1000);
+
+-- Sample Users
+-- admin
+INSERT INTO Users(username, password, role_id, activated_status) VALUES ('tephL', '123456', 1000, 1);
+-- student_voter
+INSERT INTO Users(student_id, username, email, password, role_id) VALUES 
+('1000000001','Stephen', 'stephen@example.com', '676767', 1001),
+('1000000002','Sandra', 'sandra@example.com', '676767', 1001),
+('1000000003','IannCat', 'ianncat@example.com', '676767', 1001),
+('1000000004','Justine', 'justine@example.com', '676767', 1001),
+('1000000005','MarkJoseph', 'mark@example.com', '676767', 1001),
+('1000000006','JohnPaul', 'john@example.com', '676767', 1001);
+
+
