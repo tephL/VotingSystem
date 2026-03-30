f25ee63e0da17addf93e710647a5486e1086a950
ionevenknoww <bluff4ccount@gmail.com>
2026-03-30 21:39:23 +0800

    electionControl.php
    added actions (yandere sim ahh if elses)

--- control/electionControl.php
+++ control/electionControl.php
@@ -1,5 +1,24 @@
 <?php
+include("../model/electionManager.php");
 
-$action = $_POST["action"];
+$action = $_GET['action'];
 
+if($action == "create"){
+    createElections();
+}
+else if($action == "getAll"){
+    getElection();
+}
+else if($action == "getById"){
+    getElectionById();
+}
+else if($action == "update"){
+    updateElection();
+}
+else if($action == "delete"){
+    deleteElection();
+}
+else{
+    echo "error";
+}
 ?>
