8e11819c6002609cee7badd226406e6ca641bef8
ionevenknoww <bluff4ccount@gmail.com>
2026-04-02 19:23:33 +0800

    electionControl.php
    switch case instead na if else

--- control/electionControl.php
+++ control/electionControl.php
@@ -3,25 +3,27 @@ include("../model/electionModel.php");
 
 $action = $_GET['action'];
 
-if($action == "create"){
-    createElections();
-}
-else if($action == "getAll"){
-    getElection();
-}
-else if($action == "getById"){
-    getElectionById();
-}
-else if($action == "update"){
-    updateElection();
-}
-else if($action == "delete"){
-    deleteElection();
-}
-else if($action == "getPositions"){
-    getPositionsByElection($_GET['id']);
-}
-else{
-    echo "error";
+switch ($action) {
+    case "create":
+        createElections();
+        break;
+    case "getAll":
+        getElection();
+        break;
+    case "getById":
+        getElectionById();
+        break;
+    case "update":
+        updateElection();
+        break;
+    case "delete":
+        deleteElection();
+        break;
+    case "getPositions":
+        getPositionsByElection($_GET['id']);
+        break;
+    default:
+        echo "error";
+        break;
 }
 ?>
