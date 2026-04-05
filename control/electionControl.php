a6928063aab0f67b6eebaaffb4e817902e7f8584
ionevenknoww <bluff4ccount@gmail.com>
2026-04-05 10:04:23 +0800

    electionControl.php
    Rewrite

--- control/electionControl.php
+++ control/electionControl.php
@@ -1,29 +1,73 @@
 <?php
-include("../model/electionModel.php");
 
-$action = $_GET['action'];
+require_once __DIR__ . "/../model/electionModel.php";
+
+$action = '';
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+}
 
 switch ($action) {
+    
     case "create":
+
+        if (
+            empty($_POST['title']) ||
+            empty($_POST['start']) ||
+            empty($_POST['end'])
+        ) {
+            echo "invalid";
+            exit;
+        }
+
         createElections();
         break;
+    
+    case "update":
+
+        if (
+            empty($_POST['title']) ||
+            empty($_POST['start']) ||
+            empty($_POST['end'])
+        ) {
+            echo "invalid";
+            exit;
+        }
+
+        updateElection();
+        break;
+
     case "getAll":
         getElection();
         break;
+
     case "getById":
         getElectionById();
         break;
-    case "update":
-        updateElection();
-        break;
+
     case "delete":
         deleteElection();
         break;
+
     case "getPositions":
-        getPositionsByElection($_GET['id']);
+        $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+        getPositionsByElection($id);
         break;
+
+    case "getParties":
+        $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+        getPartiesByElection($id);
+        break;
+
+    case "removeParty":
+        removeParty();
+        break;
+
     default:
-        echo "error";
+        echo json_encode([
+            "success" => false,
+            "message" => "Unknown action."
+        ]);
         break;
 }
 ?>
